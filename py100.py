660f8b6b94b702880f3dd993bd54a1403b66f789
CovertRob <robby.feconda365@gmail.com>
2024-05-08 19:58:56 -0700

    updating

--- py100.py
+++ py100.py
@@ -42,37 +42,37 @@ def local_greet(locale):
 #String exercises
 phrase = "These aren't the droids you're looking for."
 
-print(len(phrase))
+#print(len(phrase))
 
 phrase2 = 'confetti floating everywhere'
-print(phrase2.upper())
-print(phrase2)
+#print(phrase2.upper())
+#print(phrase2)
 
 name = 'Roger'
 name2 = 'DAVE'
-print(name.casefold() == 'RoGeR'.casefold())
-print(name.casefold() == name2.casefold())
+#print(name.casefold() == 'RoGeR'.casefold())
+#print(name.casefold() == name2.casefold())
 
 line = """A pirate I was meant to be!
 Trim the sails and roam the sea!"""
 
 char_sequence = 'TXkgaG92ZXJjcmFmdCBpcyBmdWxsIG9mIGVlbHMu'
 
-print('x' in char_sequence)
+#print('x' in char_sequence)
 
 def is_empty(input):
     return not input
 
-print(is_empty('mars'))
+#print(is_empty('mars'))
 
 def is_empty_or_blank(input):
     return not input or input.isspace()
 
-print(is_empty_or_blank(''))
+#print(is_empty_or_blank(''))
 
 phrase3 = 'launch school tech & talk'
 
-print(phrase3.title())
+#print(phrase3.title())
 
 def starts_with(string, prefix):
     return string.startswith(prefix)
@@ -83,3 +83,48 @@ def count_substrings(string, substring):
 scores = [96, 47, 113, 89, 100, 102]
 
 high_scores = [score for score in scores if score >= 100]
+
+#List exercises -------------------------------------------
+destinations = ['Prague', 'London', 'Sydney', 'Belfast',
+                'Rome', 'Aruba', 'Paris', 'Bora Bora',
+                'Barcelona', 'Rio de Janeiro', 'Marrakesh',
+                'New York City']
+def list_iterator(place, list):
+    for item in list:
+        if item == place:
+            return True
+        
+    return False
+
+passcode = ['11', 'jZ5', 'hQ3f*', '8!7g3', 'p3Fs']
+
+# Write some code here.
+# Expected return value: '11-jZ5-hQ3f*-8!7g3-p3Fs'
+
+# def combiner(passcode_list):
+#     combined_passcode = ''
+#     for item in passcode_list:
+#         combined_passcode = combined_passcode + item + '-'
+#     return combined_passcode
+
+# print(combiner(passcode))
+        
+passcode = ['11', 'jZ5', 'hQ3f*', '8!7g3', 'p3Fs']
+joined_passcode = ''
+
+for i in range(len(passcode)):
+    if i > 0:
+        joined_passcode += '-'
+
+    joined_passcode += passcode[i]
+
+#print(joined_passcode)  # 11-jZ5-hQ3f*-8!7g3-p3Fs
+
+grocery_list = ['paprika', 'tofu', 'garlic', 'quinoa',
+                'carrots', 'broccoli', 'hummus']
+
+for i in range(len(grocery_list)):
+    print(grocery_list.pop(0))
+    print(i)
+
+print(grocery_list)
